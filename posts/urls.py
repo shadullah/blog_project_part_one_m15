4fe7984d1cd0c0896237b48d4706e9abfabe8e9d
hexaalif <shadullahsad9@gmail.com>
2023-12-29 20:51:19 +0600

    project complete by module

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add_post/', views.add_post, name='add_post'),
-    path('edit_post/<int:id>', views.edit_post, name='edit_post')
+    path('edit_post/<int:id>', views.edit_post, name='edit_post'),
+    path('delete/<int:id>', views.delete_post, name='delete_post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . import forms
+from . import models
 
 # Create your views here.
 def add_post(req):
@@ -11,3 +12,20 @@ def add_post(req):
     else:
         post_form = forms.postForm()
     return render(req, 'add_post.html', {'form': post_form})
+
+
+def edit_post(req, id):
+    post = models.Post.objects.get(pk=id)
+    post_form = forms.postForm(instance=post)
+    
+    if req.method == 'POST':
+        post_form = forms.postForm(req.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('/')
+    return render(req, 'add_post.html', {'form': post_form})
+
+def delete_post(req, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('/')
